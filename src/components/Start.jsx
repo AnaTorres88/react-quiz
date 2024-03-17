714e5f17f6eb12de891e3325c26ab82c121eec62
unknown <ana.torresgzz@gmail.com>
2024-03-17 13:39:13 -0600

    add changes

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -3,15 +3,20 @@ import Text from "./Text/Text";
 import "./Start.css";
 /* Start page: dumb component, mostly to welcome the user*/
 export default function Start({title, intro="", instructions="", buttonText="Start Quiz", imgUrl="https://placehold.co/600x400",  onStart}) {
-    return( <section id="quiz-start">
-        <div className="title">
-            <Text type="title-big" text={title}/>
-            <Text type="title-small" text={intro}/>
+    return(
+    <section id="quiz-start" class="container">
+        <div class="row">
+            <div className="title">
+                <Text type="title-big" text={title}/>
+                <Text type="title-small" text={intro}/>
+            </div>
+            <div className="instructions">
+                <Text className="paragraph" text={instructions}/>
+                <img className="quiz-image" src={imgUrl}/>
+            </div>
         </div>
-        <div className="instructions">
-            <Text className="paragraph" text={instructions}/>
-            <img className="quiz-image" src={imgUrl}/>
-        </div>
-        <Button style = "start-button" disabled = {false} text={buttonText} onClick={onStart}/>
+       <div class="row">
+            <Button style = "start-button" disabled = {false} text={buttonText} onClick={onStart}/>
+       </div>
     </section>);
 }
